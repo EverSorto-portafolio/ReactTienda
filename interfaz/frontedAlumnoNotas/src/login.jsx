bd76a58f53639835636fde020392a6eaab4deef9
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-01-13 21:24:07 -0600

    se añade chakra  y se configura el login con el

--- interfaz/frontedAlumnoNotas/src/login.jsx
+++ interfaz/frontedAlumnoNotas/src/login.jsx
@@ -2,45 +2,66 @@ import { useState } from 'react'
 import * as API from "./services/data"
 import imagen from "./assets/login.png"
 import { useNavigate } from 'react-router'
+import { Box, Center, FormControl, FormLabel, Heading, Input, Image } from '@chakra-ui/react';
 
 
 export function Login() {
-    
+
     const [teacher, setTeacher] = useState({
         usuario: "", pass: ""
     });
-    
+
     const navigate = useNavigate();
 
     async function handleSubmit(e) {
         e.preventDefault();
 
         const response = await API.login(teacher.usuario, teacher.pass);
-        
+
         if (response == "Elemeno no encontrado") {
             alert("Usuario no encontrado");
         }
         if (response.length != 0 && response != "Elemeno no encontrado") {
-            sessionStorage.setItem("usuario", response);   
+            sessionStorage.setItem("usuario", response);
             navigate("/dashboard")
             console.log("Bienvenido nuevamente  = " + response);
         }
     }
     return (
         <>
-            <img src={imagen} />
-            <h1>Formulario desde</h1>
+            <Box mt="10rem">
+                <Center >
+                    <Image mt="3px" width="150px" height="200px" src={imagen} />
 
-            <form id='formulario' onSubmit={handleSubmit}>
-                <br></br>
-                Usuario  <input type='text' id='usuario' onChange={
-                    event => setTeacher({ ...teacher, usuario: event.target.value })} />
-                <br></br>
-                Password <input type='password' id='password'
-                    onChange={event => setTeacher({ ...teacher, pass: event.target.value })} />
-                <br></br>
-                <input type='submit' id="enviar" />
-            </form>
+                </Center>
+                <Center>
+                    <Box borderRadius="md" m="2%" width="md" id='caja' boxShadow="xl">
+                        <Box textAlign="center" >
+                            <Heading>Inicio de sesión</Heading>
+                        </Box>
+                        <Box p="20px">
+                            <form id='formulario' onSubmit={handleSubmit}>
+                                <FormControl >
+                                    <FormLabel p="20px" > Usuario </FormLabel>
+                                    <Input required type='text' id='usuario' onChange={
+                                        event => setTeacher({ ...teacher, usuario: event.target.value })} />
+                                </FormControl>
+
+                                <FormControl mt='3px'>
+                                    <FormLabel p="20px" > Password </FormLabel>
+                                    <Input required type='password' id='password'
+                                        onChange={event => setTeacher({ ...teacher, pass: event.target.value })} />
+                                </FormControl>
+
+                                <FormControl mt='3px'>
+                                    <FormLabel p="20px" > Password </FormLabel>
+                                    <Input required type='submit' id='enviar' mt='3px' borderColor='teal' value='login' />
+                                </FormControl>
+                            </form>
+                        </Box>
+                    </Box>
+                </Center>
+            </Box>
         </>
     )
 }
--- interfaz/frontedAlumnoNotas/src/main.jsx
+++ interfaz/frontedAlumnoNotas/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {App} from './App.jsx'
 import {BrowserRouter}  from 'react-router' //npm install react-router-dom localforage match-sorter sort-by
-
+import {ChakraBaseProvider, ChakraProvider} from '@chakra-ui/react'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <ChakraProvider>
     <App/>
+    </ChakraProvider>
     </BrowserRouter>
   </StrictMode>,
 )
