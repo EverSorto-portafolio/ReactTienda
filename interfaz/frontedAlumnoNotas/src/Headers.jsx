c1f153d9d3bc1d6c7527116bdf9b4ce4a8a77841
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-01-14 22:22:39 -0600

    Se aplica css al navbar

--- interfaz/frontedAlumnoNotas/src/Headers.jsx
+++ interfaz/frontedAlumnoNotas/src/Headers.jsx
@@ -1,5 +1,9 @@
+import { Box, Flex, HStack, Image} from "@chakra-ui/react";
 import {Link} from "react-router"
 import { useNavigate } from "react-router"
+import logo from "./assets/logo.png"
+
+
 export function Header(){
 
     const navigate = useNavigate();
@@ -10,9 +14,18 @@ export function Header(){
     }
     return(
         <>
-        <Link to= {"/dashboard" } > <span> Listado</span></Link> 
-        <Link to= {"/student" } > <span> Nuevo</span></Link>
-        <span onClick={()=> cerrarSesion()}>Cerrar Sesion</span>
+        <Flex w="100%"  h="36px" align="center" justify="space-between" bgColor="#11ab04" >
+        <HStack ml={"10px"} as="nav" spacing={"10px"}>
+       <Image src={logo} h="30px" ml="5px" />
+        <Link to= {"/dashboard" } > <Box _hover={{color: "gray.300"}}> Listado</Box></Link> 
+        <Link to= {"/student" } > <Box _hover={{color: "gray.300"}}> Nuevo</Box></Link>
+        </HStack>
+        <HStack as="nav" spacing={"10px"}>
+
+        </HStack>
+        <Box mr={"20px"} cursor={"pointer"} _hover={{color: "gray.300"}} onClick={()=> cerrarSesion()} > Cerrar</Box>
+        
+        </Flex>
         </>
     )
 }
