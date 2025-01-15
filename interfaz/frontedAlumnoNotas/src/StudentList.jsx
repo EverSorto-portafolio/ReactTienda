0368ac8eb6d79831177150dc26327b25a9b0e57f
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-01-14 23:23:25 -0600

    Se aplica iconos

--- interfaz/frontedAlumnoNotas/src/StudentList.jsx
+++ interfaz/frontedAlumnoNotas/src/StudentList.jsx
@@ -1,83 +1,86 @@
 import { useEffect, useState } from "react";
 import * as API from "./services/data"
 import { Link } from "react-router";
-import { Box, TableContainer, Table,Thead, Tr, Th, Tbody, Td } from "@chakra-ui/react";
+import { Box, TableContainer, Table, Thead, Tr, Th, Tbody, Td } from "@chakra-ui/react";
+import { AiFillFormatPainter } from "react-icons/ai";
+import { AiFillTrophy } from "react-icons/ai";
+import { AiOutlineDelete } from "react-icons/ai";
+
+
 
 export function StudenList() {
-   
-    
+
+
+
+
     let usuario = sessionStorage.getItem("usuario")
     console.log();
     const [students, setStudents] = useState([])
     console.log("lista de estudiantes para el profesor " + usuario)
-
-   
-
-
     useEffect(() => {
-        if(usuario){
-        API.getStudent(usuario).then(data =>{
-            console.log("Datos recibidos", data);
-            setStudents(data)
-        })
+        if (usuario) {
+            API.getStudent(usuario).then(data => {
+                console.log("Datos recibidos", data);
+                setStudents(data)
+            })
         }
-       
+
     }, [usuario])
 
-        function deleteStudent(id){
-            console.log("Resultado = "+id);
-            API.deleteStudent(id).then(
-                resultado =>{
-                    if(resultado == "true"){
-                        alert("estudiante eliminado")
-                    }else{
-                        alert("Error");
-                    }
+    function deleteStudent(id) {
+        console.log("Resultado = " + id);
+        API.deleteStudent(id).then(
+            resultado => {
+                if (resultado == "true") {
+                    alert("estudiante eliminado")
+                } else {
+                    alert("Error");
                 }
-            )
-            
-        }
+            }
+        )
 
-     return (
+    }
+
+    return (
         <>
             <Box m={"50px"}>
-             <TableContainer>
-            <Table size="md" variant="striped" colorScheme="gray">
-                <Thead>
-                    <Tr>
-                        <Th>ID</Th>
-                        <Th>DNI</Th>
-                        <Th>Nombre</Th>
-                        <Th>Direccion</Th>
-                        <Th>Edad</Th>
-                        <Th>Email</Th>
-                        <Th>Asignatura</Th>
-                        <Th></Th>
-                        <Th></Th>
-                        <Th></Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {
-                        students?.map(student => (
-                            <Tr key={student.id}>
-                             <Td>{student.id}</Td>
-                             <Td>{student.dni}</Td>
-                             <Td>{student.nombre}</Td>   
-                             <Td>{student.direccion}</Td>
-                             <Td>{student.edad}</Td>
-                             <Td>{student.email}</Td>
-                             <Td>{student.asignatura}</Td>
-                             <Td> <Link to={'/student/'+student.id}>Editar</Link>  </Td>
-                             <Td> <Link to={"/student/calificacion/" + student.matriculaId}>Calificar</Link></Td>
-                             <Td onClick={()=>deleteStudent(student.id)}>Eliminar</Td>
+                <TableContainer>
+                    <Table size="md" variant="striped" colorScheme="gray">
+                        <Thead>
+                            <Tr>
+                                <Th>ID</Th>
+                                <Th>DNI</Th>
+                                <Th>Nombre</Th>
+                                <Th>Direccion</Th>
+                                <Th>Edad</Th>
+                                <Th>Email</Th>
+                                <Th>Asignatura</Th>
+                                <Th></Th>
+                                <Th></Th>
+                                <Th></Th>
                             </Tr>
-                            )
-                        )
-                    }
-                </Tbody>
-            </Table>
-            </TableContainer>
+                        </Thead>
+                        <Tbody>
+                            {
+                                students?.map(student => (
+                                    <Tr key={student.id}>
+                                        <Td>{student.id}</Td>
+                                        <Td>{student.dni}</Td>
+                                        <Td>{student.nombre}</Td>
+                                        <Td>{student.direccion}</Td>
+                                        <Td>{student.edad}</Td>
+                                        <Td>{student.email}</Td>
+                                        <Td>{student.asignatura}</Td>
+                                        <Td> <Link ><AiFillFormatPainter to={'/student/' + student.id} /></Link> </Td>
+                                        <Td> <Link to={"/student/calificacion/" + student.matriculaId}><AiFillTrophy /></Link></Td>
+                                        <Td cursor={"pointer"}><AiOutlineDelete onClick={() => deleteStudent(student.id)} /></Td>
+                                    </Tr>
+                                )
+                                )
+                            }
+                        </Tbody>
+                    </Table>
+                </TableContainer>
             </Box>
         </>
     )
