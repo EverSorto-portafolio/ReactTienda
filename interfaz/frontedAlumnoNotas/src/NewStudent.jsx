d19e9827a8d74adbfbcbe018a041202e6ebba6a8
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-01-15 09:45:14 -0600

    Se añade formato a la vista  ingresar alumno

--- interfaz/frontedAlumnoNotas/src/NewStudent.jsx
+++ interfaz/frontedAlumnoNotas/src/NewStudent.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Header } from "./Headers";
 import * as API from "./services/data"
+import { Center, Box, Heading, FormControl, FormLabel, Input, Select } from "@chakra-ui/react";
 
 export function StudentNew(){
     const [students, setStudent] = useState ({
@@ -36,29 +37,67 @@ export function StudentNew(){
     return(
         <>
         <Header/>
-        <p>Ingresar nuevo alumno</p>
+        <Center>
+       <Box m={"40PX"} boxShadow={"xl"} borderRadius={"md"} w={"40%"} id="caja">
+        <Box textAlign={"center"}>
+            <Heading>Ingresar nuevo Alumno</Heading>
+        </Box>
+        <Box p={"20px"}>
         <form id="form-nuevoAlumno"  onSubmit={handleSubmit}>
-             DNI<input type="text" id="dni" required onChange=
-             {event => setStudent({...students,dni:event.target.value })}/>
-             nombre<input id="nombre" required onChange=
-             {event => setStudent( {...students,nombre:event.target.value })}/> <br></br>
-             Edad<input type="number" id="edad" required onChange=
-            {event => setStudent( {...students,edad:parseInt(event.target.value) })}/><br></br>
-             email<input type="text" id="email" required onChange=
-            {event => setStudent({...students,email:event.target.value })}/><br></br>
-             Direccion<input type="text" id="direccion" required onChange=
-            {event => setStudent({...students,direccion:event.target.value })}/><br></br>
-             asignatura<select  id="asignatura"  required onChange=
+             <FormControl mt={"5px"}>
+                <FormLabel>DNI</FormLabel>
+                <Input type="text" id="dni" required onChange=
+                {event => setStudent({...students,dni:event.target.value })}/>
+             </FormControl>
+             
+             <FormControl mt={"5px"}>
+                <FormLabel>Nombre</FormLabel>
+                <Input type="text" id="nombre" required onChange=
+             {event => setStudent( {...students,nombre:event.target.value })}/>
+             </FormControl>
+             
+            <FormControl mt={"5px"}>
+                <FormLabel>Edad</FormLabel>
+                <Input type="number" id="edad" required onChange=
+            {event => setStudent( {...students,edad:parseInt(event.target.value) })}/>
+             </FormControl>
+             
+             <FormControl mt={"5px"}>
+                <FormLabel>Email</FormLabel>
+                <Input type="text" id="email" required onChange=
+            {event => setStudent({...students,email:event.target.value })}/>
+             </FormControl>
+           
+             <FormControl mt={"5px"}>
+                <FormLabel>Direccion</FormLabel>
+                <Input type="text" id="direccion" required onChange=
+            {event => setStudent({...students,direccion:event.target.value })}/>
+             </FormControl>
+             
+             <FormControl mt={"5px"}>
+                <FormLabel>Direccion</FormLabel>
+                <Select  id="asignatura"  required onChange=
             {event =>  setStudent({...students,idAsignatura:parseInt(event.target.value)})}>
-               <option value='1'> matematicas</option>
+                <option value='1'> matematicas</option>
                     
-                   <option value='2'> Informatica</option>
-                   <option value='3'> Ingles</option>
-                   <option value='4'> Lenguaje</option>
-            </select>
-           <br></br>
-           <input type="submit" id="editar" value="Nuevo"/>
+                    <option value='2'> Informatica</option>
+                    <option value='3'> Ingles</option>
+                    <option value='4'> Lenguaje</option>
+                </Select>
+             </FormControl>
+           
+             <FormControl mt={"5px"}>
+                <FormLabel>Direccion</FormLabel>
+                <Input borderColor={"teal"} mt={"3px"} type="submit" id="editar" value="Nuevo" />
+                
+             </FormControl>
+           
         </form>
+        </Box>
+       </Box>
+        
+        </Center>
+        
         </>
     )
 }
